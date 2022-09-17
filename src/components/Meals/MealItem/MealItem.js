88d3819ed39c614ef659a40b05231b211d478295
Sama Mansour <samamansour306@gmail.com>
2022-09-17 18:36:07 +0300

    Add meal item form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) =>{
 const price = `$${props.price.toFixed(2)}`;
@@ -8,6 +9,9 @@ return <li className={classes.meal}>
         <h3>{props.name}</h3>
         <div>{props.description}</div>
         <div>{price}</div>
+    </div>
+    <div>
+        <MealItemForm></MealItemForm>
     </div>
 </li>
 }
