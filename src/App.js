a8e2bd2b255c84aac071be132ac329a489a75134
Sama Mansour <samamansour306@gmail.com>
2022-09-26 13:57:09 +0300

    Managing cart modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 function App() {
+  const [cartIsShown, setCartIsShown ] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
   return (
     <Fragment>
       <Cart />
