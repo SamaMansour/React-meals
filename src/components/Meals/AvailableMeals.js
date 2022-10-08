3858eb2862602ea31687e99c6cfabbdfd884e0ef
Sama Mansour <samamansour306@gmail.com>
2022-10-08 09:14:00 +0300

    Make car items removable

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,52 @@
-import Card from '../UI/card';
-import classes from './AvailableMeals.module.css'
+import Card from "../UI/card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
-import MealItem from './MealItem/MealItem';
 const DUMMY_MEALS = [
   {
     id: "m1",
-    name: "Pasta",
-    description: "tommato pasta",
-    price: 18.99,
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
   },
   {
     id: "m2",
-    name: "Rice",
-    description: "tommato Rice",
-    price: 18.99,
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
   },
   {
     id: "m3",
-    name: "Bread",
-    description: "tommato bread",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
     price: 18.99,
   },
 ];
 
-const AvailableMeals = ()=>{
-const mealsList = DUMMY_MEALS.map(meal=>(
-<MealItem
-key={meal.id}
-name={meal.name}
-description={meal.description}
-price={meal.price}
-/>));
-    return <section>
-        <Card>
-         {mealsList}   
-        </Card>
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
+  );
 };
 
 export default AvailableMeals;
